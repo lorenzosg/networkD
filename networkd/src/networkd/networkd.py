2aba40d9d2323b01ace8113f9af1497317515bd6
Lorenzo Giamartino <l.giamartino@columbia.edu>
2025-01-16 01:38:50 -0500

    first draft of rca filtering function and co-occurence dataframe function

--- networkd/src/networkd/networkd.py
+++ networkd/src/networkd/networkd.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 
+
 def prep_data(self, data):
      '''
      Intake a pandas dataframe or dictionary of two series or lists of type categorical which 
@@ -11,17 +12,23 @@ def prep_data(self, data):
     
      Parameters
      ----------
-     bipartite: pandas dataframe or dicionary of two series or lists of type categorical
+     data: pandas dataframe or dicionary of two series or lists of type categorical
 
      Returns
      -------
      adj_df: adjacency matrix as an n(# of categories) by m(# of entities) numpy array. 
      '''
 
-     if isinstance(data, dict):
-         data = pd.DataFrame(data)
+     if isinstance(data, pd.DataFrame):
+          pass
+     elif isinstance(data, dict):
+         if all(isinstance(col, list) for col in data.values()):
+            data = pd.DataFrame(data)
+         else: 
+            value_types = [type(x) for x in data.values()]
+            ValueError(f'Dictionary values must be lists. Got types {value_types} instead')
      else:
-         raise TypeError(f'data must be a pandas dataframe or dictionary. Got {type(data)}')
+         raise TypeError(f'data must be a pandas dataframe or dictionary. Got type {type(data)} instead')
 
 
      if len(data.columns) < 3:
@@ -33,12 +40,54 @@ def prep_data(self, data):
      return adj_mat
      
  
+def filter_df(data):
+    number_cat = len(set(data[0]))
+    for cat in data[0]:
+        entities = data[1][data[0] == cat]
+        cat_freq = len(data[0] == cat)
+        rca = []
+        for ent in entities:
+            categories = data[0][data[1] == ent]
+            rca_num = 1/len(categories)
+            
+            rca_denom = cat_freq/number_cat
+            rca = rca_num/rca_denom
+            if rca < 1:
+                data = data[~(data[0] == cat) & (data[1] == ent)]
+    return data
+
+            
+
+
+
+
+def co_occurence(data, rca = True):
+    dict = {}
+    unique = set(data[0])
+    for cat in unique:
+        entities = set(data[1][data[0] == cat])
+    
+        column = []
+        for cat_2 in unique:
+            if cat == cat_2:
+                column.append(1)
+            else:
+                entities_j = set(data[1][data[0] == cat_2])
+                shared = entities + entities_j
+            
+                cond_prob = shared / max(len(entities, len(entities_j)))
+                column.append(cond_prob)
         
+        dict[cat] = column
+
+            
+            
 
 
+                
 
 
-
+            
 
 
 def build_network(self, adj_df):
